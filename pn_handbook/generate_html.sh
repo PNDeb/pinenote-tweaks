3b0a827f97e459708490422022a0b84575ff1826
Maximilian Weigand <mweigand@mweigand.net>
2024-08-20 10:16:39 +0200

    Add stubs for getting-started and contributing pages

--- pn_handbook/generate_html.sh
+++ pn_handbook/generate_html.sh
@@ -4,10 +4,22 @@ outdir="${PWD}/files/etc/greeter/html"
 test -d "${outdir}" && rm -r "${outdir}"
 mkdir -p "${outdir}"
 
+cd getting_started_raw
+mkdocs build
+mkdir -p "${outdir}"/getting_started/
+rsync -avz site/ "${outdir}"/getting_started/
+cd ..
+
 cd userguide_raw
 mkdocs build
 mkdir -p "${outdir}"/user_guide/
 rsync -avz site/ "${outdir}"/user_guide/
+cd ..
+
+cd contributing_raw
+mkdocs build
+mkdir -p "${outdir}"/contributing/
+rsync -avz site/ "${outdir}"/contributing/
 cd ..
 
 # copy the landing page
---
+++ pn_handbook/getting_started_raw/docs/index.md
@@ -0,0 +1,4 @@
+# Getting Started with the PineNote
+
+
+## Introduction
