3571bb56a1820529ab199c343a9513950f0a4466
Maximilian Weigand <mweigand@mweigand.net>
2024-09-08 20:56:16 +0200

    switch to yelp for showing the help manual

--- pn_handbook/files/etc/greeter/greeter.sh
+++ pn_handbook/files/etc/greeter/greeter.sh
@@ -2,12 +2,15 @@
 
 test -e $HOME/.config/pinenote/disable_greeter && exit
 
+# use gnome-help-based yelp to show the help page
+yelp /etc/greeter/html/index.html&
 
-firefox-esr /etc/greeter/html/index.html&
-
+# check for configuration directory
 test -d $HOME/.config/pinenote || mkdir $HOME/.config/pinenote
 
+# touching this file will prevent the help from being shown on next boot/login
 touch $HOME/.config/pinenote/disable_greeter
 
+# this file should lead the Pinenote GNOME extension to disable the overview
 disable_overview_file="$HOME/.config/pinenote/do_not_show_overview"
 test -e ${disable_overview_file} && rm "${disable_overview_file}"
