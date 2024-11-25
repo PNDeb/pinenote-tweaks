96e12292737b5ddb852f043adfb123370ba1cf25
Maximilian Weigand <mweigand@mweigand.net>
2024-11-25 14:10:57 +0100

    pn_tweak_suspend_on_cover_close v1.2

--- pn_tweak_suspend_on_cover_close/debian/pn-suspend-on-cover.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the pinenote-suspend-on-cover package.
-#
-0 4	* * *	root	[ -x /usr/bin/pinenote-suspend-on-cover_maintenance ] && /usr/bin/pinenote-suspend-on-cover_maintenance
