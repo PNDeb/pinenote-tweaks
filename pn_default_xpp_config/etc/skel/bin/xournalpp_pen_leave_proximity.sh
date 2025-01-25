06e8580e57b6b7e427da0ecf77ea3defd2aa4e14
Maximilian Weigand <mweigand@mweigand.net>
2025-01-25 06:33:43 +0100

    switch xpp scripts to pn_enter_writing_mode/pn_quit_writing_mode

--- pn_default_xpp_config/etc/skel/bin/xournalpp_pen_leave_proximity.sh
+++ pn_default_xpp_config/etc/skel/bin/xournalpp_pen_leave_proximity.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+# this command is part of the pinenote_dbus_server
+pn_quit_writing_mode
 
-echo auto > /sys/devices/platform/fdec0000.ebc/power/control
-echo 0 > /sys/module/rockchip_ebc/parameters/prepare_prev_before_a2
+# echo auto > /sys/devices/platform/fdec0000.ebc/power/control
+# echo 0 > /sys/module/rockchip_ebc/parameters/prepare_prev_before_a2
 
-echo 4 > /sys/module/rockchip_ebc/parameters/default_waveform
+# echo 4 > /sys/module/rockchip_ebc/parameters/default_waveform
 
-# echo 1 > /sys/module/rockchip_ebc/parameters/diff_mode
-echo 0 > /sys/module/rockchip_ebc/parameters/split_area_limit
+# # echo 1 > /sys/module/rockchip_ebc/parameters/diff_mode
+# echo 0 > /sys/module/rockchip_ebc/parameters/split_area_limit
